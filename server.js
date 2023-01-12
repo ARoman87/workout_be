f457867fb8048935a50ed577254239c182aacd14
ARoman87 <angel427roman@gmail.com>
2023-01-12 01:00:52 -0500

    Added Server, Client and Components

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config()
 
 const express = require("express")
 const workoutRoutes = require("./routes/workouts")
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', true)
+const cors = require("cors")
 
 // Express app
 const app = express()
+app.use(cors())
 
 // middleware
 app.use(express.json())
@@ -17,8 +21,17 @@ app.use((req, res, next) => {
 // Routes
 app.use("/api/workouts", workoutRoutes)
 
-// Listen for request
-app.listen(process.env.PORT, () => {
-    console.log("listening on port ", process.env.PORT)
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for request
+        app.listen(process.env.PORT, () => {
+        console.log("Connected to db and listening on port", process.env.PORT)
 })
+    })
+    .catch((error) => {
+        console.log(error)
+    }) 
+
+
 
