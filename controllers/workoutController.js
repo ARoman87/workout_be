958bf91af08f7cc0469766801d30acf411ec9168
ARoman87 <angel427roman@gmail.com>
2023-01-23 16:24:01 -0500

    Project completed

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -30,7 +30,7 @@ const getWorkout = async (req, res) => {
 
 // create new workout
 const createWorkout = async (req, res) => {
-  const {title, load, reps} = req.body
+  const {title, load, reps, img} = req.body
 
   let emptyFields = []
 
@@ -43,6 +43,10 @@ const createWorkout = async (req, res) => {
   if(!reps) {
     emptyFields.push('reps')
   }
+  if(!img) {
+    emptyFields.push("img")
+  }
+  
   if(emptyFields.length > 0) {
     return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
   }
@@ -50,7 +54,7 @@ const createWorkout = async (req, res) => {
   // add doc to db
   try {
     const user_id = req.user._id
-    const workout = await Workout.create({title, load, reps, user_id})
+    const workout = await Workout.create({title, load, reps, img, user_id})
     res.status(200).json(workout)
   } catch (error) {
     res.status(400).json({error: error.message})
