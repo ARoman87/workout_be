87b60237553a24e0cde793541c1a3ba2eb371d87
ARoman87 <angel427roman@gmail.com>
2023-01-23 20:27:52 -0500

    npm install

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const express = require("express")
 const mongoose = require("mongoose")
-// mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', true)
 const PORT = process.env.PORT || 4000
 const cors = require("cors")
 
